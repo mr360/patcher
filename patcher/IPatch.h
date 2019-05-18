913214d028303f65cd94e41a2088897377fd714e
mr360 <qd0097@gmail.com>
2019-05-19 07:49:07 +1000

    Format the tabs

--- patcher/IPatch.h
+++ patcher/IPatch.h
@@ -11,11 +11,11 @@ namespace Patch
 
     class IPatch
     {
-    public:
-        virtual bool Load(std::fstream& file) = 0;
-        virtual void SetEA(std::string& start, std::string& end) = 0;
-        virtual bool LocateAddr(std::string& addr) = 0;
-        virtual bool Patch(std::string& esec, std::string& nsec) = 0;
-        virtual bool Unload() = 0;
+        public:
+            virtual bool Load(std::fstream& file) = 0;
+            virtual void SetEA(std::string& start, std::string& end) = 0;
+            virtual bool LocateAddr(std::string& addr) = 0;
+            virtual bool Patch(std::string& esec, std::string& nsec) = 0;
+            virtual bool Unload() = 0;
     };
 }
