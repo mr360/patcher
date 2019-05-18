b1a1a170243442333805f22a482779276a1d22e7
mr360 <qd0097@gmail.com>
2019-05-19 07:41:37 +1000

    Created patch interface

--- patcher/IPatch.h
+++ patcher/IPatch.h
@@ -1,2 +1,21 @@
 #pragma once
+#include <string>
+#include <fstream>
 
+namespace Patch
+{
+    struct Obj
+    {
+        int a = 0;
+    };
+
+    class IPatch
+    {
+    public:
+        virtual bool Load(std::fstream& file) = 0;
+        virtual void SetEA(std::string& start, std::string& end) = 0;
+        virtual bool LocateAddr(std::string& addr) = 0;
+        virtual bool Patch(std::string& esec, std::string& nsec) = 0;
+        virtual bool Unload() = 0;
+    };
+}
